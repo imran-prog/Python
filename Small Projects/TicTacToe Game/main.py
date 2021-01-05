cb0d37e7d6756f526d6bbf7e85edea4cb59276a8
Imran Akbar <infotainer04@gmail.com>
2021-01-05 23:22:02 +0500

    Editted TicTacToe game and solved some bugs in it

--- Small Projects/TicTacToe Game/main.py
+++ Small Projects/TicTacToe Game/main.py
@@ -1,47 +1,46 @@
+""""""""""""""""""""""""""""""""" TicTacToe Game """""""""""""""""""""""""""""""""""""""
+# Installing Modules
 import random
 import time
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 
 def drawBoard(board):
+    # Board Design
+    print("\n" + board[1] + " | " + board[2] + " | " + board[3] + "     1 | 2 | 3")
+    print("--|---|--")
+    print(board[4] + " | " + board[5] + " | " + board[6] + "     4 | 5 | 6")
+    print("--|---|--")
+    print(board[7] + " | " + board[8] + " | " + board[9] + "     7 | 8 | 9" + "\n")
 
-    print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-    print('   |   |')
-    print('-----------')
-    print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('   |   |')
-
-def inputPlayerLetter():
+def inputPlayerLetter():                        # Selecting the symbol to use in the game by the user
     letter = ''
     while not (letter == 'X' or letter == 'O'):
-        print('Do you want to be X or O?')
+        print(Fore.BLUE + 'Do you want to by X or O?')
         letter = input().upper()
 
     if letter == 'X':
         return ['X', 'O']
     else:
         return ['O', 'X']
-
+0
 def whoGoesFirst():
-    # if random.randint(0, 1) == 0:
+    # if random.randint(0, 1) == 0:             # Toss to select who plays first
     #     return 'computer'
     # else:
     #     return 'player'
 
     return 'computer'
 
-def playAgain():
-    print('Do you want to play again? (yes or no)')
+def playAgain():                                # To ask user to play the game again
+    print(Fore.GREEN + 'Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
-def makeMove(board, letter, move):
+def makeMove(board, letter, move):              # Important Func = To enter the symbol in the position on board
     board[move] = letter
 
-def isWinner(bo, le):
+def isWinner(bo, le):                           # To see whether the user\computer have won the game or not
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
     (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
     (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
@@ -51,21 +50,20 @@ def isWinner(bo, le):
     (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
     (bo[9] == le and bo[5] == le and bo[1] == le)) # diagonal
 
-def getBoardCopy(board):
+def getBoardCopy(board):                        # To show the board again and again on the terminal
     dupeBoard = []
-
     for i in board:
         dupeBoard.append(i)
 
-
     return dupeBoard
-def isSpaceFree(board, move):
+
+def isSpaceFree(board, move):                   # Check whether the space where user\computer chose is available
     return board[move] == ' '
 
-def getPlayerMove(board):
+def getPlayerMove(board):                       # To get the move from the player
     move = ' '
     while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
-        print('What is your next move? (1-9)')
+        print(Fore.CYAN + 'What is your next move? (1-9)')
         move = input()
 
     return int(move)
@@ -117,27 +115,149 @@ def getComputerMove(board, computerLetter):
     # Move on one of the sides.
     return chooseRandomMoveFromList(board, [2, 4, 6, 8])
 
-def isBoardFull(board):
+def isBoardFull(board):                         # Check whether the board is full or not
     for i in range(1, 10):
         if isSpaceFree(board, i):
             return False
     return True
 
 def rule():
-    print('It seems you have some Trouble while play game, now worry i will help you.')
-    print('In Tic-Tac-Toe you have to make a straight line out of 3-O or 3-X')
-    print('For example:')
-    print(" X | O | X \n ---------- \n X | X | O \n ----------\n X | O | O \n")
+    print(Fore.GREEN  + 'It seems you have some Trouble while play game, no worry i will help you.')
+    print(Fore.GREEN + 'In Tic-Tac-Toe you have to make a straight line out of 3-O or 3-X')
+    print(Fore.GREEN + 'For example:')
+    print(Fore.GREEN + " X | O | X \n ---------- \n X | X | O \n ----------\n X | O | O \n")
+
+def play_tournament():
+    duo_ai = input(Fore.BLUE + 'Would you link to play tournament with your friend or bot(f/b): ')
+    if duo_ai == 'friend' or duo_ai == 'f' or duo_ai == 'my friend' or duo_ai == 'duo' or duo_ai == 'human':
+        i = 1
+        p = 0
+        c = 0
+        print(Fore.GREEN + 'Hello, I am A.I. robot and i am your umpire today.')
+        u_name = input(Fore.GREEN + 'What should i call you player 1(X)? ')
+        u2_name = input(Fore.GREEN + 'What should i call you player 2(O)? ')
+        print(Fore.GREEN + "Let's see who wins today.")
+        print(Fore.GREEN + "Let's start with the game now", u_name.upper(), 'VS', u2_name.upper(), '\n\n')
+        while i <= 3:
+            gameIsPlaying = True
+            # Reset the board
+            theBoard = [' '] * 10
+            playerLetter, computerLetter = ['X', 'O']
+            
+            if random.randint(0, 1) == 0:
+                turn = u_name
+            else:
+                turn =  u2_name
+            print('The ' + turn + ' will go first.')
+            
+
+            while gameIsPlaying:
+                if i > 3 : gameIsPlaying = False
+                if turn == u_name:
+                    # Player's turn.
+                    drawBoard(theBoard)
+                    print(u_name)
+                    move = getPlayerMove(theBoard)
+                    makeMove(theBoard, playerLetter, move)
+
+                    if isWinner(theBoard, playerLetter):
+                        drawBoard(theBoard)
+                        print(f'Hurray! {u_name} have won the game!')
+                        p += 1
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            drawBoard(theBoard)
+                            print('Oh! the game is a tie!\nTry hard for next time')
+                            break
+                        else:
+                            turn = u2_name
+
+                else:
+                    # Player's turn.
+                    drawBoard(theBoard)
+                    print(u2_name)
+                    move = getPlayerMove(theBoard)
+                    makeMove(theBoard, computerLetter, move)
+
+                    if isWinner(theBoard, computerLetter):
+                        drawBoard(theBoard)
+                        print(f'Hurray! {u2_name} have won the game!')
+                        c += 1
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            drawBoard(theBoard)
+                            print('The game is a tie!\nTry hard for next time')
+                            break
+                        else:
+                            turn = u_name
+
+            i += 1
+
+        if p > c: print("Congrats!!! "+u_name + " has won the tournament\n")
+        elif p == c: print("The tournament is a tie\n")
+        else: print("Congrats!!! "+u2_name + " has won the tournament\n")
+    elif duo_ai == 'computer' or duo_ai == 'bot' or duo_ai == 'ai' or duo_ai == 'single' or duo_ai == 'b':
+        i = 1
+        p = 0
+        c = 0
+        while i <= 3:
+            gameIsPlaying = True
+            # Reset the board
+            theBoard = [' '] * 10
+            playerLetter, computerLetter = inputPlayerLetter()
+            turn = whoGoesFirst()
+            print('The ' + turn + ' will go first.')
+            while gameIsPlaying:
+                if i > 3 : gameIsPlaying = False
+                if turn == 'player':
+                    # Player's turn.
+                    drawBoard(theBoard)
+                    move = getPlayerMove(theBoard)
+                    makeMove(theBoard, playerLetter, move)
+                    if isWinner(theBoard, playerLetter):
+                        drawBoard(theBoard)
+                        print(Fore.GREEN + 'Hurray! You have won the game!')
+                        p += 1
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            drawBoard(theBoard)
+                            print(Fore.RED + 'The game is a tie!\nTry hard for next time')
+                            break
+                        else:
+                            turn = 'computer'
+                else:
+                    # Computer's turn.
+                    move = getComputerMove(theBoard, computerLetter)
+                    makeMove(theBoard, computerLetter, move)
+                    if isWinner(theBoard, computerLetter):
+                        drawBoard(theBoard)
+                        print(Fore.RED +  'The computer has beaten you!\nYou lose.')
+                        c += 1
+                        gameIsPlaying = False
+                    else:
+                        if isBoardFull(theBoard):
+                            drawBoard(theBoard)
+                            print(Fore.GREEN + 'The game is a tie!\nTry hard for next time')
+                            break
+                        else:
+                            turn = 'player'
+            i += 1
+        if p > c: print("Player has won the tournament")
+        elif p == c: print("The tournament is a tie\n")
+        else: print("Computer has won the tournament")
 
 def play_with_human():
     global u_name, u2_name, theBoard
     game_on = True
-    print('Hello, I am A.I. robot and i am your umpire today.')
-    u_name = input('What should i call you player 1(X)? ')
-    u2_name = input('What should i call you player 2(O)? ')
-    print("let's see who wins today.")
-    print("let's start with the game now", u_name.upper(), 'vs', u2_name.upper(), '\n\n')
-    print("Type 'Rule' to see Rules.")
+    print(Fore.GREEN + 'Hello, I am A.I. robot and i am your umpire today.')
+    u_name = input(Fore.GREEN + 'What should i call you player 1(X)? ')
+    u2_name = input(Fore.GREEN + 'What should i call you player 2(O)? ')
+    print(Fore.GREEN + "Let's see who wins today.")
+    print(Fore.GREEN + "Let's start with the game now", u_name.upper(), 'VS', u2_name.upper(), '\n\n')
+    print(Fore.GREEN + "Type 'Rule' to see Rules.")
     while True:
         # Reset the board
         theBoard = [' '] * 10
@@ -148,7 +268,6 @@ def play_with_human():
             turn =  u2_name
         print('The ' + turn + ' will go first.')
         gameIsPlaying = True
-
         while gameIsPlaying:
             if turn == u_name:
                 # Player's turn.
@@ -156,19 +275,17 @@ def play_with_human():
                 print(u_name)
                 move = getPlayerMove(theBoard)
                 makeMove(theBoard, playerLetter, move)
-
                 if isWinner(theBoard, playerLetter):
                     drawBoard(theBoard)
-                    print(f'Hooray! {u_name} have won the game!')
+                    print(f'Hurray! {u_name} have won the game!')
                     gameIsPlaying = False
                 else:
                     if isBoardFull(theBoard):
                         drawBoard(theBoard)
-                        print('The game is a tie!')
+                        print('The game is a tie!\nTry hard for next time')
                         break
                     else:
                         turn = u2_name
-
             else:
                 # Player's turn.
                 drawBoard(theBoard)
@@ -178,35 +295,35 @@ def play_with_human():
 
                 if isWinner(theBoard, computerLetter):
                     drawBoard(theBoard)
-                    print(f'Hooray! {u2_name} have won the game!')
+                    print(f'Hurray! {u2_name} have won the game!')
                     gameIsPlaying = False
                 else:
                     if isBoardFull(theBoard):
                         drawBoard(theBoard)
-                        print('The game is a tie!')
+                        print('The game is a tie!\nTry hard for next time')
                         break
                     else:
                         turn = u_name
-
         if not playAgain():
             break
 
-print('Welcome to Tic Tac Toe!')
+print(Fore.YELLOW + 'Welcome to Tic Tac Toe !!!')
 time.sleep(0.5)
-print('Start the game be typing * start or game * or\n')
-print('type * -h or help * to see how to play this Game...\n')
+print(Fore.MAGENTA + 'Start the game be typing * start or game * or play tournament * tour *\n')
+print(Fore.MAGENTA + 'Type * -h or help * to see how to play this Game...\n')
 time.sleep(0.5)
 while True:
     input_1 = input('>Tic-Tac-Toe> ').lower()
     if input_1 == '-h' or input_1 == 'help' or input_1 == 'options':
-        print('Welcome to Help box:)')
-        print('Here are all options and everything you can do with this Game.\n')
+        print(Fore.GREEN + 'Welcome to Help box:)')
+        print(Fore.GREEN + 'Here are all options and everything you can do with this Game.\n')
         print('-----------------------------------------------------------------------')
         print('| Options                 | Use                                       |')
         print('-----------------------------------------------------------------------')
         print('| -h or help              | To see all available options for the game.|')
         print('''| about                   | To see information about the developer of |
 |                         | this Game                                 |''')
+        print('| tour                    | To play the tournament.                   |')
         print('| start or play           | To start a new game.                      |')
         print('| rule                    | To Know rules of the game.                |')
         print('| clear                   | To Clear the Terminal Screen.             |')
@@ -214,17 +331,17 @@ while True:
         print('-----------------------------------------------------------------------\n')
     elif input_1 == 'about':
         print('Hello,')
-        print('      I an Tic-Tac-Toe game which is created by Imran')
-        print('Check out my Insta: @unknown.domination')
+        print('      This Tic-Tac-Toe game is created by Humayun Bajwa\nA newbie student of Comsats University Islamabad')
     elif input_1 == 'exit' or input_1 == 'quit':
         exit()
     elif input_1 == 'clear':
         print(chr(27) + "[2J")
+    elif input_1 == 'tour':
+        play_tournament()
     elif input_1 == 'start' or input_1 == 'play' or input_1 == 'game' or input_1 == 'run':
-        duo_ai = input('Would you link to play with your friend or bot(f/b): ')
+        duo_ai = input(Fore.BLUE + 'Would you link to play with your friend or dare to challenge our bot(f/b): ')
         if duo_ai == 'friend' or duo_ai == 'f' or duo_ai == 'my friend' or duo_ai == 'duo' or duo_ai == 'human':
             play_with_human()
-
         elif duo_ai == 'computer' or duo_ai == 'bot' or duo_ai == 'ai' or duo_ai == 'single' or duo_ai == 'b':
             while True:
                 # Reset the board
@@ -233,7 +350,6 @@ while True:
                 turn = whoGoesFirst()
                 print('The ' + turn + ' will go first.')
                 gameIsPlaying = True
-
                 while gameIsPlaying:
                     if turn == 'player':
                         # Player's turn.
@@ -243,16 +359,15 @@ while True:
 
                         if isWinner(theBoard, playerLetter):
                             drawBoard(theBoard)
-                            print('Hooray! You have won the game!')
+                            print(Fore.GREEN + 'Hurray! You have won the game!')
                             gameIsPlaying = False
                         else:
                             if isBoardFull(theBoard):
                                 drawBoard(theBoard)
-                                print('The game is a tie!')
+                                print(Fore.RED + 'The game is a tie!\nTry hard for next time')
                                 break
                             else:
                                 turn = 'computer'
-
                     else:
                         # Computer's turn.
                         move = getComputerMove(theBoard, computerLetter)
@@ -260,19 +375,18 @@ while True:
 
                         if isWinner(theBoard, computerLetter):
                             drawBoard(theBoard)
-                            print('The computer has beaten you! You lose.')
+                            print(Fore.RED +  'The computer has beaten you! \n You lose.')
                             gameIsPlaying = False
                         else:
                             if isBoardFull(theBoard):
                                 drawBoard(theBoard)
-                                print('The game is a tie!')
+                                print(Fore.GREEN + 'The game is a tie!\nTry hard for next time')
                                 break
                             else:
                                 turn = 'player'
-
                 if not playAgain():
                     break
     elif input_1 == 'rule' or input_1 == 'rules':
         rule()
     else:
-        print("It's seems some error occurred, please check your spellings.")
+        print(Fore.RED + "It's seems some error occurred, Please check your input.")
